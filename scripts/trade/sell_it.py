f8e9c3e239dffd35751c3d8bd5bb7b06e9a29c6e
Zach Dicklin <zachdicklin@google.com>
2024-01-11 13:59:31 -0700

    autosell

--- scripts/trade/sell_it.py
+++ scripts/trade/sell_it.py
@@ -8,10 +8,12 @@ import datetime
 from dotenv import load_dotenv
 
 # Constants
-CSV_FIELDNAMES = ['STATUS', 'ACTION', 'COINBASE_PRODUCT_ID', 'TRIGGER', 'TRIGGER_PRICE_USD', 'SELL_PRICE_USD', 'QUANTITY']
-FLOAT_FIELDS = ['TRIGGER_PRICE_USD', 'SELL_PRICE_USD', 'QUANTITY']
+CSV_FIELDNAMES = ['STATUS','ACTION','COINBASE_PRODUCT_ID','QUANTITY','PURCHASE_PRICE','STOP_LOSS_PERCENT','PROFIT_PERCENT']
+FLOAT_FIELDS = ['QUANTITY','PURCHASE_PRICE','STOP_LOSS_PERCENT','PROFIT_PERCENT']
 DATE_FORMAT = "%Y%m%d%H%M%S"
 TIME_SLEEP_SECONDS = 10
+RAISE_STOPLOSS_THRESHOLD = 1.015
+RAISE_STOPLOSS_VALUE = 0.99
 
 # Load environment variables
 load_dotenv()
@@ -42,11 +44,39 @@ def build_client_order_id(order):
     return f"{order['action']}_{symbol}_{timestamp}"
 
 def print_order_details(order, exchange_rate_usd, client_order_id):
-    print(f"{client_order_id}\t{order['coinbase_product_id']}\t bid is ${exchange_rate_usd:08.4f}\t{order['trigger']}\ttrigger at ${order['trigger_price_usd']:08.4f}\tto sell {order['quantity']:08.4f} @ ${order['sell_price_usd']:08.4f}")
+    print(f"{order['coinbase_product_id']}\t bid is ${exchange_rate_usd:08.4f}\tpurchase ${order['purchase_price']:08.4f}\tdelta {delta_since_purchase(order, exchange_rate_usd):0.2f}%\tlow ${stop_loss_price(order):08.4f}\thigh ${profit_price(order):08.4f}")
+
+def stop_loss_price(order):
+    return order['purchase_price'] * order['stop_loss_percent']
+
+def profit_price(order):
+    return order['purchase_price'] * order['profit_percent']
+
+def delta_since_purchase(order, exchange_rate_usd):
+    return 100 * (exchange_rate_usd - order['purchase_price']) / order['purchase_price']
 
 def should_trigger_order(order, exchange_rate_usd):
-    return ((order['trigger'] == 'PRICE_DROPS_TO' and exchange_rate_usd <= order['trigger_price_usd']) or
-            (order['trigger'] == 'PRICE_RAISES_TO' and exchange_rate_usd >= order['trigger_price_usd']))
+    return exchange_rate_usd >= profit_price(order) or exchange_rate_usd <= stop_loss_price(order)
+
+def adjust_stoploss(order, exchange_rate_usd):
+    prev_value = order['stop_loss_percent']
+    next_value = order['stop_loss_percent']
+
+    if exchange_rate_usd >= order['purchase_price'] * 1.01:
+        next_value = 0.98
+
+    # if exchange_rate_usd >= order['purchase_price'] * 1.02:
+    #     next_value = 1.0
+
+    if exchange_rate_usd >= order['purchase_price'] * 1.025:
+        next_value = ((exchange_rate_usd - order['purchase_price']) / order['purchase_price']) + 1 - 0.005
+
+    return max(next_value, prev_value)
+    # if exchange_rate_usd <= order['purchase_price'] * RAISE_STOPLOSS_THRESHOLD:
+    #     return order['stop_loss_percent']
+
+    # adjusted = (((exchange_rate_usd - order['purchase_price']) / order['purchase_price'] + 1) * RAISE_STOPLOSS_VALUE)
+    # return max(order['stop_loss_percent'], adjusted)
 
 def main(csv_filename):
     while True:
@@ -54,24 +84,29 @@ def main(csv_filename):
         print(datetime.datetime.now().strftime("%H:%M:%S"))
 
         for order in orders:
-            if order['status'] != 'OPEN':
-                continue
+            if order['status'] == 'OPEN':
+                try:
+                    best_bid_ask = client.get_best_bid_ask([order['coinbase_product_id']])
+                    exchange_rate_usd = float(best_bid_ask.pricebooks[0].bids[0].price)
 
-            best_bid_ask = client.get_best_bid_ask([order['coinbase_product_id']])
-            exchange_rate_usd = float(best_bid_ask.pricebooks[0].bids[0].price)
+                    client_order_id = build_client_order_id(order)
+                    print_order_details(order, exchange_rate_usd, client_order_id)
 
-            client_order_id = build_client_order_id(order)
-            print_order_details(order, exchange_rate_usd, client_order_id)
-
-            if should_trigger_order(order, exchange_rate_usd):
-                coinbase_order = client.create_limit_order(
-                    client_order_id=client_order_id,
-                    product_id=order['coinbase_product_id'],
-                    side=Side.SELL,
-                    limit_price=exchange_rate_usd,
-                    base_size=order['quantity'])
-                print(coinbase_order)
-                order['status'] = 'TRIGGERED'
+                    if should_trigger_order(order, exchange_rate_usd):
+                        coinbase_order = client.create_limit_order(
+                            client_order_id=client_order_id,
+                            product_id=order['coinbase_product_id'],
+                            side=Side.SELL,
+                            limit_price=exchange_rate_usd,
+                            base_size=order['quantity'])
+                        print(coinbase_order)
+                        order['status'] = 'TRIGGERED'
+                    else:
+                        order['stop_loss_percent'] = adjust_stoploss(order, exchange_rate_usd)
+                except Exception as e:
+                    print("An error occurred:", e)
+            elif order['status'] == 'TRIGGERED':
+                print(f"{order['coinbase_product_id']}\t TRIGGERED")
 
         write_orders_to_csv(orders, csv_filename)
         time.sleep(TIME_SLEEP_SECONDS)
