4059255a87c5fab86c1e5b933f08166cc94f2706
Zach <zdicklin@gmail.com>
2024-01-18 11:41:41 -0700

    Longer prediction loop

--- bin/lstm_train.sh
+++ bin/lstm_train.sh
@@ -3,5 +3,5 @@ while true; do
     scp zach@144.202.24.235:/home/zach/btctools/crypto_exchange_rates.csv ./data/
     python3 ./scripts/analysis/analysis_lstm_train_deltas.py ./data/crypto_exchange_rates.csv ./data/lstm_predictions.csv ./models/lstm.h5
     scp ./data/lstm_predictions.csv zach@144.202.24.235:/home/zach/btctools/lstm_predictions.csv
-    sleep 60
+    sleep 300
 done
