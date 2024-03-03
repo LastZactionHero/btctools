53f8c75a8cbaa3507c00d2f7729f2d1ad509e337
user <user@site.com>
2024-03-02 18:32:51 -0700

    Fix order

--- live_broker_scratch.py
+++ live_broker_scratch.py
@@ -19,52 +19,14 @@ engine = init_db_engine("./db/live.db")
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Assuming your Orders table is mapped to a class named 'Order'
-session.query(Order).filter(Order.id.in_([13, 14, 15, 16, 17])).delete()
-session.commit()  # Important: Commit the changes
+orders_to_update = session.query(Order).filter(Order.id.in_([140,141,142,143,144,149])).all()
+
+# Update the stop_loss_percent for each fetched order
+for order in orders_to_update:
+    order.stop_loss_percent = 0.782
+    order.recovery_mode = True
+
+session.commit()
+
+# Close the session
 session.close()
-#engine = init_db_engine("./db/live.db")
-#Session = sessionmaker(bind=engine)
-#session = Session()
-
-#orders_to_delete = session.query(Order).filter(Order.id.in_([13, 14, 15, 16, 17]))
-
-# Delete the queried records
-#session.delete(orders_to_delete)
-
-# Commit the changes to the database
-#session.commit()
-#
-#session.close()
-
-#logger = logging.getLogger('LiveLogger')
-#broker = Broker(logger, {'live_trades': True})
-
-#print(broker.usdc_available())
-
-#b#roker.buy("PREP_001", "GFI-USDC", 50, 1.2834)
-# broker.sell("PREP_002", "BADGER-USDC", 1, 4.36)
-# prices = broker.prices()
-# ask = prices.ask("BTRST")
-# ask = 0.915556923992
-# x = broker.buy("PREP_007", "BTRST-USDC", 5.102022011, ask)
-# # print(x)
-# DB_FILENAME = "./db/live.db"
-# engine = init_db_engine(DB_FILENAME)
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-# def order_summary(order):
-#     return {
-#         "symbol": order.coinbase_product_id.split("-")[0],
-#         "ordered_at": order.created_at,
-#         "status": order.status
-#     }
-# new_buys = session.query(Order).filter(and_(Order.status == "OPEN", Order.created_at > datetime.now() - timedelta(days=1))).all()
-# new_sells = session.query(Order).filter(and_(Order.status == "SOLD", Order.sold_at > datetime.now() - timedelta(days=1))).all()
-
-# print(list(map(order_summary, new_buys)))
-# print(list(map(order_summary, new_sells)))
-
-
-# session.close()
